c0a1efc8ae572484e6ce2b50b31fb8245c95940b
Rithu Y J <rithuyj08@gmail.com>
2026-04-05 19:28:56 +0530

    Finance Backend

--- config/db.js
+++ config/db.js
@@ -6,6 +6,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres", // Type of database
   protocol: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 // Export connection
